0095d05f57bd44dd34fc22006e350799dd3fdf2f
Jortoise <email@example.com>
2026-06-18 16:43:42 +0800

    update

--- getdata.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0whv-job-tracker"
-python run_scheduler.py --run-once
-pause
--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0whv-job-tracker"
-start "" python web\app.py
-timeout /t 2 /nobreak >nul
-start "" "http://127.0.0.1:5000"
